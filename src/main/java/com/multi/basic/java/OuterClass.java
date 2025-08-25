54377b9915e5a04bf49336ede98e92effe02f450
이응수 <sistsay@gmail.com>
2025-08-25 14:36:45 +0900

    inner

--- src/main/java/com/multi/basic/java/OuterClass.java
+++ src/main/java/com/multi/basic/java/OuterClass.java
@@ -5,12 +5,19 @@ public class OuterClass {
     public OuterClass() {//외부생성자
         System.out.println("OuterClass()");
     }
+    private void showOuter(){
+        System.out.println("InnerClass.showOuter()");
+    }
     public class InnerClass {
         private int innerValue;
+
         public InnerClass() {// 내부생성자
             System.out.println("InnerClass()");
             innerValue = 1;//자기 자신을 private라도 접근가능
             outerValue=10;//내부에서 외부에 instance변수나 Method를 private이어도 접근 가능
+        }
+        public void showInner(){//innerClass Method
+            showOuter();//OuterClass Method Call
         }
     }
 
