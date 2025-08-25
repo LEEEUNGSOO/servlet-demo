16a44cb9436145b66a10cdab1537a7ad1f56d7f3
이응수 <sistsay@gmail.com>
2025-08-25 14:31:55 +0900

    innerClass

---
+++ src/main/java/com/multi/basic/java/OuterClass.java
@@ -0,0 +1,23 @@
+package com.multi.basic.java;
+//MemberInnerClass
+public class OuterClass {
+    private int outerValue;
+    public OuterClass() {//외부생성자
+        System.out.println("OuterClass()");
+    }
+    public class InnerClass {
+        private int innerValue;
+        public InnerClass() {// 내부생성자
+            System.out.println("InnerClass()");
+            innerValue = 1;//자기 자신을 private라도 접근가능
+            outerValue=10;//내부에서 외부에 instance변수나 Method를 private이어도 접근 가능
+        }
+    }
+
+    public static void main(String[] args) {
+        //new InnerClass(); 내부 생성만 불가
+        OuterClass.InnerClass inner = new OuterClass().new InnerClass();
+
+    }
+
+}
