110c747ddd845d8900d1073bb1690ffbc1c91d0c
이응수 <sistsay@gmail.com>
2025-09-03 14:33:29 +0900

    mybatis1

--- src/main/java/com/multi/dto/BoardDTO.java
+++ src/main/java/com/multi/dto/BoardDTO.java
@@ -1,4 +1,16 @@
 package com.multi.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data  //getter,setter,toString
 public class BoardDTO {
+    private int id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
 }
+
