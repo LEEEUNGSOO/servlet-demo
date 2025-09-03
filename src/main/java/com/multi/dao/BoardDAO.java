bf34eb936f49def47bd2fd172c44e21637ee5cd3
이응수 <sistsay@gmail.com>
2025-09-03 16:28:17 +0900

    mybatis5

--- src/main/java/com/multi/dao/BoardDAO.java
+++ src/main/java/com/multi/dao/BoardDAO.java
@@ -12,4 +12,25 @@ public class BoardDAO {
             return s.selectList("com.multi.dao.BoardDAO.getAllBoards");
         }
     }
+    public BoardDTO getBoardById(int id) {
+        try (SqlSession s = MyBatisUtil.getSqlSessionFactory().openSession()) {
+            return s.selectOne("com.multi.dao.BoardDAO.getBoardById", id);
+        }
+    }
+    public int insertBoard(BoardDTO b) {
+        try (SqlSession s = MyBatisUtil.getSqlSessionFactory().openSession(true)) {
+            return s.insert("com.multi.dao.BoardDAO.insertBoard", b);
+        }
+    }
+    public int updateBoard(BoardDTO b) {
+        try (SqlSession s = MyBatisUtil.getSqlSessionFactory().openSession(true)) {
+            return s.update("com.multi.dao.BoardDAO.updateBoard", b);
+        }
+    }
+    public int deleteBoard(int id) {
+        try (SqlSession s = MyBatisUtil.getSqlSessionFactory().openSession(true)) {
+            return s.delete("com.multi.dao.BoardDAO.deleteBoard", id);
+        }
+    }
 }
+
