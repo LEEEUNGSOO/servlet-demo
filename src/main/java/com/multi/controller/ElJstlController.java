ff31183fa325b180e219099f272632b6690e6249
이응수 <sistsay@gmail.com>
2025-09-04 13:52:12 +0900

    jstl

--- src/main/java/com/multi/controller/ElJstlController.java
+++ src/main/java/com/multi/controller/ElJstlController.java
@@ -1,5 +1,7 @@
 package com.multi.controller;
 
+import com.multi.dao.BoardDAO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +15,14 @@ public class ElJstlController extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         String action = req.getParameter("action");
-        if(action!=null&& action.equals("jstlLesson1")) {//
-            System.out.println(action);
+
+        if(action!=null&& action.equals("redirect")) {//
+            BoardDAO boardDAO = new BoardDAO();
+           req.setAttribute("list",  boardDAO.getAllBoards());
+           resp.sendRedirect(req.getContextPath() + "/jstl/redirectJstl.jsp");
+        }else if(action!=null&& action.equals("forward")) {
+            req.getRequestDispatcher("jstl/forwardJstl.jsp").forward(req, resp);
+
         }
 
 
--- src/main/webapp/eljstl/redirectJstl.jsp
+++ src/main/webapp/eljstl/redirectJstl.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: sists
   Date: 2025-09-04
-  Time: 오후 1:33
+  Time: 오후 1:32
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,7 +11,8 @@
     <title>Title</title>
 </head>
 <body>
-<h1>ForwardJstl</h1>
+<h1>REDIRECT</h1>
+<%=request.getAttribute("list")%>
 ${list}
 </body>
 </html>
