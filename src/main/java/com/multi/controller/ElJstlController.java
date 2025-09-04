3b9315b47947c73043299b38f60e9dcdca094c0f
이응수 <sistsay@gmail.com>
2025-09-04 13:37:27 +0900

    jstl lesson

--- src/main/java/com/multi/controller/ElJstlController.java
+++ src/main/java/com/multi/controller/ElJstlController.java
@@ -1,4 +1,23 @@
 package com.multi.controller;
 
-public class ElJstlController {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/jstlLesson")
+public class ElJstlController extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String action = req.getParameter("action");
+        if(action!=null&& action.equals("jstlLesson1")) {//
+            System.out.println(action);
+        }
+
+
+
+    }
 }
